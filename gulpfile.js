408d12a292b828448eccdb14bf3c0953ef0681e1
Andreas Lundahl <mail@andreaslundahl.com>
2016-02-20 15:29:39 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
 var babelify = require("babelify");
 var browserify = require("browserify");
+var gulp = require("gulp");
+var less = require("gulp-less");
 var source = require("vinyl-source-stream");
 
-gulp.task("default", function () {
-  browserify({entries: "./src/core.jsx", extensions: [".jsx"], debug: true})
+gulp.task("js", function () {
+  browserify({entries: "./src/jsx/core.jsx", extensions: [".jsx"], debug: true})
     .transform(babelify.configure({presets: ["es2015", "react"]}))
     .bundle()
-    .pipe(source("out.js"))
-    .pipe(gulp.dest("out"));
+    .pipe(source("core.js"))
+    .pipe(gulp.dest("out/js"));
+});
+
+gulp.task("css", function () {
+  gulp.src("./src/less/core.less")
+    .pipe(less())
+    .pipe(gulp.dest("./out/css"));
+});
+
+gulp.task("default", function () {
+  gulp.watch("./src/less/**/*.less", ["css"]);
+  gulp.watch("./src/jsx/**/*.jsx", ["js"]);
 });
